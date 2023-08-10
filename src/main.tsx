0fe6364f426ee9313aa4c475b8e1f0aac4366ea3
Odirlei <odirlei_sabella@hotmail.com>
2023-08-10 19:27:33 -0300

    Aula 4 - Componetização do header

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //Componentes
-import Home from "./pages/home"
-import ListaServicos from "./pages/listaServicos"
-import Footer from "./components/footer"
+import Home from "./pages/Home"
+import ListaServicos from "./pages/ListaServicos"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 //CSS
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header/>
     <ListaServicos />
     <Footer/>
   </React.StrictMode>,
